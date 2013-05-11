63c7ee588a33298eff3f813a0331bc445a29d8a9
ingk1 <keith.ing1@gmail.com>
2013-05-12 00:31:16 +1000

    Operational code with limited waveform library and self-defined initial estimate covariance.
    Optimal waveform selection technique is based on minimisation of mean square track error.

--- matlab/linear_model.m
+++ matlab/linear_model.m
@@ -1,4 +1,4 @@
-function [x, y] = linear_model(x1, F, G, H, w, n, N)
+function [x] = linear_model(x1, P0, F, Q, G, N) 
 
 % x1 is the initial state [range; velocity; acceleration]
 % F is the target motion model matrix e.g. [speed; velocity; acceleration]
@@ -9,27 +9,28 @@ function [x, y] = linear_model(x1, F, G, H, w, n, N)
 % N is the number of iterations
 
 % Generate target flight (with constant acceleration)
-x = zeros(3, N);        % target state vector (range; range-rate; acceleration)
-y = zeros(2, N);        % measurement vector  (range; range-rate)
+x = zeros(3, N+1);        % target state vector (range; range-rate; acceleration)
+% y = zeros(2, N);        % measurement vector  (range; range-rate)
 
 
 % Generate process noise
 
-sigma = sqrt(w);                    % target model process noise(stdev of target accl (m/s^2))
+%sigma = sqrt(w);                    % target model process noise(stdev of target accl (m/s^2))
 % w = zeros(3, N);                    % target trajectory noise
-w = diag(sigma)*randn(3,N);              % generate acceleration noise ~N(0,sigma^2) (iid)
+w = (G*chol(Q))'*randn(3,N);
+% w = chol(Q)'*randn(3,N);              % generate acceleration noise ~N(0,sigma^2) (iid)
 % w(1:2,:) = [dt^2/2; dt]*w(3,:);     % calc range and range rate noise
 
 % Generate measurement noise
-sigma = n;                          % measurement model noise [phase noise, frequency shift noise]
-n = diag(sigma)*randn(2,N);         % measurement noise vector
+%sigma = n;                          % measurement model noise [phase noise, frequency shift noise]
+%n = diag(sigma)*randn(2,N);         % measurement noise vector
+% n = chol(R)'*randn*2,N);
 
 % Target State Model
-x(:,1) = x1 + G*w(:,1);             % target state vector (r, rr, ra)
-y(:,1) = H*x(:,1) + n(:,1);         % measurement vector of target state (r, rr)
+x(:,1) = x1 + chol(P0)'*randn(3,1);%w(:,1);             % target state vector (r, rr, ra)
     
-for k = 1:N-1    
-    x(:,k+1) = F*x(:,k) + G*w(:,k); % target state vector (r, rr, ra)
-    y(:,k+1) = H*x(:,k) + n(:,k);   % measurement vector of target state (r, rr)
+for k = 2:N+1    
+    x(:,k) = F*x(:,k-1) + w(:,k-1); % target state vector (r, rr, ra)
+%     y(:,k-1) = H*x(:,k) + n(:,k-1);   % measurement vector of target state (r, rr)
 end
 
